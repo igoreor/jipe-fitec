6b38c40fa0f8bc7fb7ce519abbf5d2c16bf5a49f
rikcav <henriquearau000@gmail.com>
2024-02-16 18:12:45 -0300

    chore: initial commit

---
+++ src/main/java/br/upe/ecoenergy/EcoenergyApplication.java
@@ -0,0 +1,13 @@
+package br.upe.ecoenergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoenergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoenergyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/upe/ecoenergy/EcoenergyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upe.ecoenergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoenergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
