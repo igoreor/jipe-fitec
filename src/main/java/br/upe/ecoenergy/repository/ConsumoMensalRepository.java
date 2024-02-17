253077e9fe60ca521f448a0faab434e9adc190e9
rikcav <henriquearau000@gmail.com>
2024-02-17 19:03:08 -0300

    fix: ajuste de erro no repositório ConsumoMensalRepository

--- src/main/java/br/upe/ecoenergy/repository/ConsumoMensalRepository.java
+++ src/main/java/br/upe/ecoenergy/repository/ConsumoMensalRepository.java
@@ -1,6 +1,7 @@
 package br.upe.ecoenergy.repository;
 
+import br.upe.ecoenergy.domain.ConsumoMensal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ConsumoMensalRepository extends JpaRepository<ConsumoMensalRepository, Long> {
+public interface ConsumoMensalRepository extends JpaRepository<ConsumoMensal, Long> {
 }
