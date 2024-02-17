2d723f8f2490c3649c831f4b6a7c1703f6e7388d
rikcav <henriquearau000@gmail.com>
2024-02-17 17:53:08 -0300

    feat: entidade consumo mensal

--- src/main/java/br/upe/ecoenergy/domain/ConsumoMensal.java
+++ src/main/java/br/upe/ecoenergy/domain/ConsumoMensal.java
@@ -1,4 +1,23 @@
 package br.upe.ecoenergy.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "consumo_mensal")
 public class ConsumoMensal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private BigDecimal kwhTotal;
+
+    private BigDecimal custoTotal;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
 }
