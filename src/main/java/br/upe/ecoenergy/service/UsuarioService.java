d5dde47f84ee59a36e6cc2558ec20e087adca0b2
rikcav <henriquearau000@gmail.com>
2024-02-17 19:13:16 -0300

    feat: método para buscar um usuário por id

--- src/main/java/br/upe/ecoenergy/service/UsuarioService.java
+++ src/main/java/br/upe/ecoenergy/service/UsuarioService.java
@@ -1,4 +1,24 @@
 package br.upe.ecoenergy.service;
 
+import br.upe.ecoenergy.domain.Usuario;
+import br.upe.ecoenergy.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UsuarioService {
+    public final UsuarioRepository repository;
+
+    public Usuario buscarUsuarioPorId(Long id) {
+        Optional<Usuario> usuario = repository.findById(id);
+
+        if (usuario.isEmpty()) {
+            throw new RuntimeException("Não foi encontrado usuário com esse ID.");
+        }
+
+        return usuario.get();
+    }
 }
