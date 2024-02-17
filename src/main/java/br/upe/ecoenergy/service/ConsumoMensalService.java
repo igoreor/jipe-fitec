ce796bb8c923b253c520a024990e3f93d42f3075
rikcav <henriquearau000@gmail.com>
2024-02-17 19:28:39 -0300

    feat: buscar consumo mensal por id

--- src/main/java/br/upe/ecoenergy/service/ConsumoMensalService.java
+++ src/main/java/br/upe/ecoenergy/service/ConsumoMensalService.java
@@ -1,4 +1,24 @@
 package br.upe.ecoenergy.service;
 
+import br.upe.ecoenergy.domain.ConsumoMensal;
+import br.upe.ecoenergy.repository.ConsumoMensalRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class ConsumoMensalService {
+    private final ConsumoMensalRepository repository;
+
+    public ConsumoMensal buscarConsumoMensalPorId(Long id) {
+        Optional<ConsumoMensal> consumoMensal = repository.findById(id);
+
+        if (consumoMensal.isEmpty()) {
+            throw new RuntimeException("Não foi encontrado um consumo mensal com esse ID.");
+        }
+
+        return consumoMensal.get();
+    }
 }
