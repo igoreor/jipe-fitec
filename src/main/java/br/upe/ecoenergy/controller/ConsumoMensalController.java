72d0cfc8c8803c4039ef4cdd8e48c9fee8860ba4
rikcav <henriquearau000@gmail.com>
2024-02-20 15:28:07 -0300

    feat: endpoint para listar todos os consumos mensais

--- src/main/java/br/upe/ecoenergy/controller/ConsumoMensalController.java
+++ src/main/java/br/upe/ecoenergy/controller/ConsumoMensalController.java
@@ -1,4 +1,23 @@
 package br.upe.ecoenergy.controller;
 
+import br.upe.ecoenergy.domain.ConsumoMensal;
+import br.upe.ecoenergy.service.ConsumoMensalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/consumos-mensais")
+@RequiredArgsConstructor
 public class ConsumoMensalController {
+
+    private final ConsumoMensalService consumoMensalService;
+
+    @GetMapping
+    public ResponseEntity<List<ConsumoMensal>> listarConsumosMensais() {
+        List<ConsumoMensal> consumosMensais = consumoMensalService.consumosMensais();
+        return ResponseEntity.ok(consumosMensais);
+    }
 }
