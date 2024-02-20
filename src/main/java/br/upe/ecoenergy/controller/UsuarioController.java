285b567924931af6b495a8f2b951b5ec1174fa53
rikcav <henriquearau000@gmail.com>
2024-02-20 15:26:26 -0300

    feat: endpoint para registrar um novo usuário

--- src/main/java/br/upe/ecoenergy/controller/UsuarioController.java
+++ src/main/java/br/upe/ecoenergy/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package br.upe.ecoenergy.controller;
 import br.upe.ecoenergy.domain.Usuario;
 import br.upe.ecoenergy.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,5 +18,11 @@ public class UsuarioController {
     public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable Long id) {
         Usuario usuario = usuarioService.buscarUsuarioPorId(id);
         return ResponseEntity.ok(usuario);
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> registrarUsuario(@RequestBody Usuario usuario) {
+        Usuario novoUsuario = usuarioService.registrarUsuario(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
     }
 }
