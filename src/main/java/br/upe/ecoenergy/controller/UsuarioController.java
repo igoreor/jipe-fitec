4b9eff3654201d177887d6885737e8f61beb43dd
rikcav <henriquearau000@gmail.com>
2024-02-20 15:25:30 -0300

    feat: endpoint para buscar usuário por id

--- src/main/java/br/upe/ecoenergy/controller/UsuarioController.java
+++ src/main/java/br/upe/ecoenergy/controller/UsuarioController.java
@@ -1,4 +1,21 @@
 package br.upe.ecoenergy.controller;
 
+import br.upe.ecoenergy.domain.Usuario;
+import br.upe.ecoenergy.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuarios")
+@RequiredArgsConstructor
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable Long id) {
+        Usuario usuario = usuarioService.buscarUsuarioPorId(id);
+        return ResponseEntity.ok(usuario);
+    }
 }
