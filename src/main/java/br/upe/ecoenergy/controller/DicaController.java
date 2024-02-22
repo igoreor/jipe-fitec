9258e83b0f392388fe62216b4b50d3133282210a
igoreor <igoreor24@gmail.com>
2024-02-21 21:49:02 -0300

    adicionando o crud dicas

--- src/main/java/br/upe/ecoenergy/controller/DicaController.java
+++ src/main/java/br/upe/ecoenergy/controller/DicaController.java
@@ -1,4 +1,48 @@
 package br.upe.ecoenergy.controller;
 
+import br.upe.ecoenergy.domain.Dica;
+import br.upe.ecoenergy.service.DicaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/dicas")
+@RequiredArgsConstructor
 public class DicaController {
+
+    private final DicaService dicaService;
+
+    @GetMapping
+    public ResponseEntity<List<Dica>> listarDicas() {
+        List<Dica> dicas = dicaService.listarDicas();
+        return ResponseEntity.ok(dicas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Dica> buscarDicaPorId(@PathVariable Long id) {
+        Dica dica = dicaService.buscarDicaPorId(id);
+        return ResponseEntity.ok(dica);
+    }
+
+    @PostMapping
+    public ResponseEntity<Dica> adicionarDica(@RequestBody Dica dica) {
+        Dica novaDica = dicaService.adicionarDica(dica);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaDica);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Dica> atualizarDica(@PathVariable Long id, @RequestBody Dica dicaAtualizada) {
+        Dica dica = dicaService.atualizarDica(id, dicaAtualizada);
+        return ResponseEntity.ok(dica);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarDica(@PathVariable Long id) {
+        dicaService.deletarDica(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/br/upe/ecoenergy/service/DicaService.java
+++ src/main/java/br/upe/ecoenergy/service/DicaService.java
@@ -1,4 +1,47 @@
 package br.upe.ecoenergy.service;
 
+import br.upe.ecoenergy.domain.Dica;
+import br.upe.ecoenergy.repository.DicaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class DicaService {
+    private final DicaRepository repository;
+
+    public List<Dica> listarDicas() {
+        return repository.findAll();
+    }
+
+    public Dica buscarDicaPorId(Long id) {
+        Optional<Dica> dica = repository.findById(id);
+
+        if (dica.isEmpty()) {
+            throw new RuntimeException("Não foi encontrada uma dica com este ID.");
+        }
+
+        return dica.get();
+    }
+
+    public Dica adicionarDica(Dica dica) {
+        return repository.save(dica);
+    }
+
+    public Dica atualizarDica(Long id, Dica dicaAtualizada) {
+        Dica dica = buscarDicaPorId(id);
+
+        dica.setTitulo(dicaAtualizada.getTitulo());
+        dica.setDescricao(dicaAtualizada.getDescricao());
+
+        return adicionarDica(dica);
+    }
+
+    public void deletarDica(Long id) {
+        Dica dica = buscarDicaPorId(id);
+        repository.delete(dica);
+    }
 }
